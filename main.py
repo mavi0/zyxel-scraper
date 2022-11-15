45cd8bfaf1d9727124b6685b7f50cb6fafecb362
Eleanor Davies <ellie@mavieson.co.uk>
2022-11-15 17:32:31 +0000

    imsi

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Scrape:
         self.__cpe_uname = os.environ.get('CPE_UNAME', "root")
         self.__cpe_passwd = os.environ.get('CPE_PASSWD', "root")
         self.__cpe_hostname = os.environ.get('CPE_HOSTNAME', "192.168.1.1")
-        self.__imsi = os.environ.get('IMSI', "DEFAULT")
+        self.__imsi = os.environ.get('IMSI', "DEFAULT_IMSI")
         self.__interval = int(os.environ.get('INTERVAL', 300))
         self.__output = {}
         self.__output["client_id"] = self.__client_id
